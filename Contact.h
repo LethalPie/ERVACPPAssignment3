cdd37512aee3f2c4efcf9daaf532a96f220a0175
Eliphalet Read <eliphaletread@outlook.com>
2023-11-29 10:51:50 -0400

    Init
    Init repo with starter code provided by Nick Muise (Prof)

---
+++ Contact.h
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+#ifndef ASSIGNMENT_3_CONTACT_H
+#define ASSIGNMENT_3_CONTACT_H
+
+
+class Contact {
+private:
+    std::string* m_name { nullptr };
+    int m_age {};
+    std::vector<Contact*> m_friends {};
+    Contact(std::string* name, int age);
+    void addFriend(Contact* newFriend);
+    void removeFriend(std::string_view friendName);
+};
+
+
+#endif //ASSIGNMENT_3_CONTACT_H
