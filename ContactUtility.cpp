b658809edd0d5f19efbab8531c530adfced0a9f7
Eliphalet Read <eliphaletread@outlook.com>
2023-12-05 17:22:20 -0400

    Completed Asingment

--- ContactUtility.cpp
+++ ContactUtility.cpp
@@ -4,7 +4,7 @@ void ContactUtility::sortContacts(std::vector<Contact*>& contacts) {
 
     std::sort(contacts.begin(),
               contacts.end(),
-              [] (Contact *a, Contact *b) { return a->m_friends > b->m_friends; });
+              [] (Contact *a, Contact *b) { return *a->m_name < *b->m_name; });
 }
 
 void ContactUtility::addContact(std::vector<Contact*>& contacts) {
@@ -22,47 +22,38 @@ void ContactUtility::addContact(std::vector<Contact*>& contacts) {
 
 void ContactUtility::printContacts(std::vector<Contact*>& contacts) {
 
+    sortContacts(contacts);
+
+    std::cout << "\n**Printing Sorted Contacts**\n";
+    for (const auto& contact : contacts) {
+        std::cout << "Name: " << *contact->m_name << ", Age: " << contact->m_age << "\n";
+    }
 }
 
 void ContactUtility::addFriend(std::vector<Contact*>& contacts) {
 
     std::cout << "Contact that wants to add: ";
-    Contact* conAPtr {findContact(contacts)};
+    Contact* conAPtr { findContact(contacts) };
     if (!conAPtr) return;
 
     std::cout << "Who would " << *conAPtr->m_name << " like to add?: ";
-    Contact* conFPtr {findContact(contacts)};
+    Contact* conFPtr { findContact(contacts) };
     if (!conFPtr) return;
 
     conAPtr->addFriend(conFPtr);
-
-    std::cout << *conFPtr->m_name << " added to " << *conAPtr->m_name << "'s friend list.";
-
 }
 
 void ContactUtility::printFriends(std::vector<Contact*>& contacts) {
 
     std::cout << "Enter the name of the contact whose friend list you want to view: ";
-    std::string friendName;
-    std::cin >> friendName;
+    Contact* conCPtr { findContact(contacts) };
+    if (!conCPtr) return;
 
-    // Find the contact in the list
-    auto it = std::find_if(contacts.begin(), contacts.end(),
-                           [&friendName](Contact *c) { return *c->m_name == friendName; });
-
-    if (it == contacts.end()) {
-        std::cout << "Contact does not exist. Closing.\n";
-        return;
-    }
-
-    Contact* selectedContact = *it;
-    const auto& friends = selectedContact->m_friends;
-
-    if (friends.empty()) {
-        std::cout << friendName << " has no friends.\n";
-    } else {
-        std::cout << "\nFriends of " << friendName << ":\n";
-        for (const auto& friendContact : friends) {
+    if (conCPtr->m_friends.empty())
+        std::cout << *conCPtr->m_name << " has no friends.\n";
+    else {
+        std::cout << "\n**Friends of " << *conCPtr->m_name << "**\n";
+        for (const auto& friendContact : conCPtr->m_friends) {
             std::cout << "   - " << *friendContact->m_name << "\n";
         }
     }
@@ -71,7 +62,7 @@ void ContactUtility::printFriends(std::vector<Contact*>& contacts) {
 void ContactUtility::deleteContact(std::vector<Contact*>& contacts) {
 
     std::cout << "Which contact would you like to delete?: ";
-    Contact* conDPtr {findContact(contacts)};
+    Contact* conDPtr { findContact(contacts) };
     if (!conDPtr) return;
 
     std::cout << "Removing from friend lists...\n";
